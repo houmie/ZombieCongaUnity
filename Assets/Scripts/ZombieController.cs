1b68c8577e38397a6323bae941a224296f692db6
Hooman Korasani <hooman@venuscloud.com>
2014-09-12 11:59:15 +0100

    Finished Part 4

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -7,9 +7,43 @@ public class ZombieController : MonoBehaviour {
 	public float turnSpeed;
 	private Vector3 moveDirection;
 
+	[SerializeField]
+	private PolygonCollider2D[] colliders;
+	private int currentColliderIndex = 0;
+
 	// Use this for initialization
 	void Start () {
 		moveDirection = Vector3.right;
+	}
+
+	private void EnforceBounds()
+	{
+		// 1
+		Vector3 newPosition = transform.position; 
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+		
+		// 2
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
+		float xMax = cameraPosition.x + xDist;
+		float xMin = cameraPosition.x - xDist;
+
+		float yMax = mainCamera.orthographicSize;
+		
+		if (newPosition.y < -yMax || newPosition.y > yMax) {
+			newPosition.y = Mathf.Clamp( newPosition.y, -yMax, yMax );
+			moveDirection.y = -moveDirection.y;
+		}
+		
+		// 3
+		if ( newPosition.x < xMin || newPosition.x > xMax ) {
+			newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+			moveDirection.x = -moveDirection.x;
+		}
+		// TODO vertical bounds
+		
+		// 4
+		transform.position = newPosition;
 	}
 	
 	// Update is called once per frame
@@ -17,18 +51,32 @@ public class ZombieController : MonoBehaviour {
 		Vector3 currentPosition = transform.position;
 		if (Input.GetButton ("Fire1")) {
 			Vector3 moveToward = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
 			moveDirection = moveToward - currentPosition;
 			moveDirection.z = 0;
 			moveDirection.Normalize();
-
-			Vector3 target = moveDirection * moveSpeed + currentPosition;
-			transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
 		}
+		
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+		
 		float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 		transform.rotation = 
 			Quaternion.Slerp( transform.rotation, 
 			                 Quaternion.Euler( 0, 0, targetAngle ), 
 			                 turnSpeed * Time.deltaTime );
+		EnforceBounds();
+	}
+
+	public void SetColliderForSprite( int spriteNum )
+	{
+		colliders[currentColliderIndex].enabled = false;
+		currentColliderIndex = spriteNum;
+		colliders[currentColliderIndex].enabled = true;
+	}
+
+	void OnTriggerEnter2D( Collider2D other )
+	{
+		Debug.Log ("Hit " + other.gameObject);
 	}
 }
+
